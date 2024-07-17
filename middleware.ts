c1a4b29426be6cf4391747fdb3de7f137eafae03
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-07-16 21:08:40 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
