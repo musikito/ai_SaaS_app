c66406f6e51c3ce882f36282db018f5c20e6adb4
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-07-16 20:53:01 -0400

    webhooks

--- middleware.ts
+++ middleware.ts
@@ -7,14 +7,24 @@
 //   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 // };
 
+// import { authMiddleware } from "@clerk/nextjs/server";
+// // This example protects all routes including api/trpc routes
+// // Please edit this to allow other routes to be public as needed.
+// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// export default authMiddleware({
+//   publicRoutes: ["/", "/api/webhooks/clerk"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
 import { authMiddleware } from "@clerk/nextjs/server";
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/clerk"],
+  ignoredRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
