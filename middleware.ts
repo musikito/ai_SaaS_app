dbd59c8e256864271b17b3dfc5cd2f65c3d7734c
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-07-16 21:38:01 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
-});
+export default clerkMiddleware(
+  publicRoutes: ["/", "/api/webhooks/clerk"],
+);
 
-// Stop Middleware running on static files
-export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)',};
-
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
